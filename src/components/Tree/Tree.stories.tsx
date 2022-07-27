f46cccde90e125d044011f5b4e4992518927fd4a
ponyorange <63995931@qq.com>
2022-07-28 07:13:58 +0800

    完善tree组件

--- src/components/Tree/Tree.stories.tsx
+++ src/components/Tree/Tree.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Tree, { DataNode, TreeProps } from "./tree";
@@ -47,7 +47,7 @@ const IntroTemplate: ComponentStory<typeof Tree> = () => {
           key: "0-0-1",
           children: [
             {
-              title: <span style={{ color: "#1890ff" }}>sss</span>,
+              title: <span style={{ color: "orange" }}>sss</span>,
               key: "0-0-1-0",
             },
           ],
@@ -71,7 +71,7 @@ const IntroTemplate: ComponentStory<typeof Tree> = () => {
       onSelect={onSelect}
       onCheck={onCheck}
       treeData={treeData}
-      // checkable
+      checkable
     />
   );
 };
@@ -79,7 +79,91 @@ const IntroTemplate: ComponentStory<typeof Tree> = () => {
 export const Introduction = IntroTemplate.bind({});
 Introduction.storyName = "基本的Tree";
 
-const SizeTemplate: ComponentStory<typeof Tree> = (args) => <div>待实现</div>;
+const SizeTemplate: ComponentStory<typeof Tree> = (args) => {
+  const treeData: DataNode[] = [
+    {
+      title: "0-0",
+      key: "0-0",
+      children: [
+        {
+          title: "0-0-0",
+          key: "0-0-0",
+          children: [
+            { title: "0-0-0-0", key: "0-0-0-0" },
+            { title: "0-0-0-1", key: "0-0-0-1" },
+            { title: "0-0-0-2", key: "0-0-0-2" },
+          ],
+        },
+        {
+          title: "0-0-1",
+          key: "0-0-1",
+          children: [
+            { title: "0-0-1-0", key: "0-0-1-0" },
+            { title: "0-0-1-1", key: "0-0-1-1" },
+            { title: "0-0-1-2", key: "0-0-1-2" },
+          ],
+        },
+        {
+          title: "0-0-2",
+          key: "0-0-2",
+        },
+      ],
+    },
+    {
+      title: "0-1",
+      key: "0-1",
+      children: [
+        { title: "0-1-0-0", key: "0-1-0-0" },
+        { title: "0-1-0-1", key: "0-1-0-1" },
+        { title: "0-1-0-2", key: "0-1-0-2" },
+      ],
+    },
+    {
+      title: "0-2",
+      key: "0-2",
+    },
+  ];
+
+  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([
+    "0-0-0",
+    "0-0-1",
+  ]);
+  const [checkedKeys, setCheckedKeys] = useState<React.Key[]>(["0-0-0"]);
+  const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
+  const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
+
+  const onExpand = (expandedKeysValue: React.Key[]) => {
+    console.log("onExpand", expandedKeysValue);
+    // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+    // or, you can remove all expanded children keys.
+    setExpandedKeys(expandedKeysValue);
+    setAutoExpandParent(false);
+  };
+
+  const onCheck = (checkedKeysValue: React.Key[]) => {
+    console.log("onCheck", checkedKeysValue);
+    setCheckedKeys(checkedKeysValue);
+  };
+
+  const onSelect = (selectedKeysValue: React.Key[], info: any) => {
+    console.log("onSelect", info);
+    setSelectedKeys(selectedKeysValue);
+  };
+
+  return (
+    <Tree
+      checkable
+      onExpand={onExpand}
+      expandedKeys={expandedKeys}
+      autoExpandParent={autoExpandParent}
+      onCheck={onCheck}
+      checkedKeys={checkedKeys}
+      onSelect={onSelect}
+      selectedKeys={selectedKeys}
+      treeData={treeData}
+    />
+  );
+};
 
 export const Size = SizeTemplate.bind({});
 Size.storyName = "受控操作示例";
--- src/components/Tree/tree.tsx
+++ src/components/Tree/tree.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 import Icon from "../Icon";
 import Checkbox from "../Checkbox";
 import Transition from "../Transition/transition";
@@ -17,15 +17,23 @@ export type DataNode = {
 export interface TreeProps {
   /** 节点前添加 Checkbox 复选框 */
   checkable?: boolean;
+  /** 自动展开父节点 */
+  autoExpandParent?: boolean;
   /** 默认展开指定的树节点 */
   defaultExpandedKeys?: string[];
   /** 默认选中的树节点 */
   defaultSelectedKeys?: string[];
   /** 默认选中复选框的树节点 */
   defaultCheckedKeys?: string[];
+  /** （受控）展开指定的树节点 */
+  expandedKeys?: React.Key[];
+  /** （受控）选中复选框的树节点 */
+  checkedKeys?: React.Key[];
+  /** （受控）设置选中的树节点 */
+  selectedKeys?: React.Key[];
   /** 点击复选框触发 */
   onSelect?: (
-    selectedKeys: string,
+    selectedKeys: React.Key[],
     e: {
       selected: boolean;
       selectedNodes: DataNode;
@@ -35,7 +43,7 @@ export interface TreeProps {
   ) => void;
   /** 点击树节点触发 */
   onCheck?: (
-    checkedKeys: string,
+    checkedKeys: React.Key[],
     e: {
       checked: boolean;
       checkedNodes: DataNode;
@@ -44,6 +52,8 @@ export interface TreeProps {
       halfCheckedKeys: string[];
     }
   ) => void;
+  /** 节点展开/关闭时触发 */
+  onExpand?: (expandedKeysValue: React.Key[]) => void;
   /** treeNodes 数据，如果设置则不需要手动构造 TreeNode 节点（key 在整个树范围内唯一） */
   treeData: DataNode[];
 }
@@ -66,6 +76,8 @@ export const Tree: React.FC<TreeProps> = (props) => {
   const [treeDataStare, setTreeDataStare] = useState(treeData);
   const treeDataMap: Record<string, any> = useRef({});
 
+  const [checkedKeys, setCheckedKeys] = useState<string[]>(["0-0"]);
+
   const subTreeTringleClick = (e: React.MouseEvent) => {
     const clickKey = e.currentTarget.getAttribute("data-key") as string;
     treeDataMap.current[clickKey].isOpen =
@@ -85,9 +97,57 @@ export const Tree: React.FC<TreeProps> = (props) => {
         if (item.isOpen === undefined) {
           item.isOpen = true;
         }
+
+        const subtreeItemClasses = classNames("orange-tree-list-subtree-item", {
+          "orange-tree-list-subtree-item-disabled": !!item.disabled,
+        });
         const angleClasses = classNames("orange-tree-list-subtree-angle", {
           "orange-tree-list-subtree-angle-close": !item.isOpen,
         });
+
+        const checkboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+          console.log(item.key, e.target.checked);
+          if (e.target.checked) {
+            //添加所有儿子孙子
+            const newCKs = [item.key, ...checkedKeys];
+            const queue = [item.children];
+            while (queue.length > 0) {
+              const child = queue.shift();
+              child?.forEach((c) => {
+                if (c.children) queue.push(c.children);
+                if (!newCKs.includes(c.key)) {
+                  newCKs.push(c.key);
+                }
+              });
+            }
+            setCheckedKeys(newCKs);
+          } else {
+            //移除所有儿子孙子
+            const newCKs = checkedKeys.filter((fit) => fit !== item.key);
+            const queue = [item.children];
+            while (queue.length > 0) {
+              const child = queue.shift();
+              child?.forEach((c) => {
+                if (c.children) queue.push(c.children);
+                if (newCKs.includes(c.key)) {
+                  newCKs.splice(newCKs.indexOf(c.key), 1);
+                }
+              });
+            }
+            setCheckedKeys(newCKs);
+          }
+        };
+
+        let subtreeChecked = true;
+        let subtreeIndeterminate = false;
+        item.children.forEach((citem) => {
+          if (checkedKeys.includes(citem.key)) {
+            subtreeIndeterminate = true;
+          } else {
+            subtreeChecked = false;
+          }
+        });
+
         return (
           <div
             key={item.key}
@@ -104,15 +164,20 @@ export const Tree: React.FC<TreeProps> = (props) => {
               {/* 节点复选框 */}
               {checkable && (
                 <span
-                  className="orange-tree-list-subtree-item"
+                  className="orange-tree-list-checkbox"
                   data-title={item.title}
                   data-key={item.key}
                 >
-                  <Checkbox />
+                  <Checkbox
+                    disabled={!!item.disableCheckbox || !!item.disabled}
+                    onchange={checkboxChange}
+                    checked={subtreeChecked}
+                    indeterminate={subtreeIndeterminate}
+                  />
                 </span>
               )}
               <span
-                className="orange-tree-list-subtree-item"
+                className={subtreeItemClasses}
                 data-title={item.title}
                 data-key={item.key}
                 onClick={treeTitleClick}
@@ -126,15 +191,40 @@ export const Tree: React.FC<TreeProps> = (props) => {
           </div>
         );
       } else {
+        const itemClasses = classNames(
+          "orange-tree-treeNode",
+          "orange-tree-list-item",
+          {
+            "orange-tree-treeNode-disabled": !!item.disabled,
+          }
+        );
+
+        const checkboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+          console.log(item.key, e.target.checked);
+          if (e.target.checked) {
+            setCheckedKeys([item.key, ...checkedKeys]);
+          } else {
+            setCheckedKeys(checkedKeys.filter((it) => it !== item.key));
+          }
+        };
+
         return (
-          <div
-            key={item.key}
-            className="orange-tree-treeNode orange-tree-list-item"
-          >
+          <div key={item.key} className={itemClasses}>
+            {/* 三角形占位符 */}
+            <span
+              className="orange-tree-list-subtree-angle"
+              style={{ visibility: "hidden" }}
+            >
+              <Icon icon="caret-down" />
+            </span>
             {/* 节点复选框 */}
             {checkable && (
-              <span>
-                <Checkbox />
+              <span className="orange-tree-list-checkbox">
+                <Checkbox
+                  disabled={!!item.disableCheckbox || !!item.disabled}
+                  onchange={checkboxChange}
+                  checked={checkedKeys.includes(item.key)}
+                />
               </span>
             )}
             <span
